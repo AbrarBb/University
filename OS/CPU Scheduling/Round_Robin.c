ccfd1669bde4df5e0a10ea1fda63b61e91f6b30e
Abrar khatib Lajim <88080003+AbrarBb@users.noreply.github.com>
2025-04-18 20:33:32 +0600

    Update Round_Robin.c

--- OS/CPU Scheduling/Round_Robin.c
+++ OS/CPU Scheduling/Round_Robin.c
@@ -1,36 +1,40 @@
 #include <stdio.h>
 int main() 
 {
-    int n, tq;
+    int n, tq, a, t=0;
     printf("Processes: ");
     scanf("%d", &n);
     printf("Time quantum: ");
     scanf("%d", &tq);
-    int bt[n], rt[n], wt[n]={0}, tat[n], i, t=0;
+    int bt[n], rt[n], wt[n]={0}, tat[n];
     printf("Burst times:\n");
-    for(i=0;i<n;i++) { scanf("%d", &bt[i]); rt[i] = bt[i]; }
+    for(a=0; a<n; a++) {
+        scanf("%d", &bt[a]);
+        rt[a] = bt[a];
+    }
 
     int done;
     do {
         done = 1;
-        for(i=0; i<n; i++) {
-            if(rt[i] > 0) {
+        for(a=0; a<n; a++) {
+            if(rt[a] > 0) {
                 done = 0;
-                if(rt[i] > tq) {
+                if(rt[a] > tq) {
                     t += tq;
-                    rt[i] -= tq;
+                    rt[a] -= tq;
                 } else {
-                    t += rt[i];
-                    wt[i] = t - bt[i];
-                    rt[i] = 0;
+                    t += rt[a];
+                    wt[a] = t - bt[a];
+                    rt[a] = 0;
                 }
             }
         }
     } while(!done);
 
-    for(i=0;i<n;i++) tat[i] = wt[i] + bt[i];
+    for(a=0; a<n; a++)
+        tat[a] = wt[a] + bt[a];
 
     printf("P\tBT\tWT\tTAT\n");
-    for(i=0;i<n;i++)
-        printf("P%d\t%d\t%d\t%d\n", i+1, bt[i], wt[i], tat[i]);
+    for(a=0; a<n; a++)
+        printf("P%d\t%d\t%d\t%d\n", a+1, bt[a], wt[a], tat[a]);
 }
