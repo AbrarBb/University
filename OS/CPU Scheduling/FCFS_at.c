a9a0266c55de0fddd547b01eeb21242563dfe51a
Abrar khatib Lajim <88080003+AbrarBb@users.noreply.github.com>
2025-04-21 03:52:26 +0600

    Update FCFS_at.c
    Added average WT,TAT depends on AT

--- OS/CPU Scheduling/FCFS_at.c
+++ OS/CPU Scheduling/FCFS_at.c
@@ -26,11 +26,18 @@ int main()
     tat[a] = wt[a] + bt[a];
   }
 
-
 printf("\n    WT\tTAT\n");
   for(a=0; a<n; a++)
   {
       printf("P%d\t%d\t %d\n", a+1, wt[a], tat[a]);
   }
-    
+  
+    //Average 
+    int tat_s=0,wt_s=0;
+    for(int a=0;a<n;a++)
+    {
+        tat_s=tat[a]+tat_s;
+        wt_s=wt_wt[a];
+    }
+    printf("\n AvgWT:%.2f\tAvg.TAT:%.2f",(float)wt_s/n,(float)tat_s/n);
 }
