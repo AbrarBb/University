8772a6600dd63dc0edfa4e990260f5c4f1b1b077
Abrar khatib Lajim <88080003+AbrarBb@users.noreply.github.com>
2025-04-21 04:40:08 +0600

    Create SRTF_at.c

---
+++ OS/CPU Scheduling/SRTF_at.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main() 
+{
+  int n, a, at[10], bt[10], rt[10], wt[10], tat[10], done=0, t=0;
+  printf("Enter number of processes: ");
+  scanf("%d", &n);
+  
+  printf("   AT BT\n");
+  for(a=0; a<n; a++) 
+  {
+    printf("P%d: ", a+1);
+    scanf("%d %d", &at[a], &bt[a]);
+    
+  }
+  while(done < n) 
+  {
+    int min = 9999, p = -1;
+    for(a = 0; a < n; a++)
+      if(at[a] <= t && rt[a] > 0 && rt[a] < min)
+        min = rt[a], p = a;
+    if(p == -1) { t++; continue; }
+
+    rt[p]--;
+    if(rt[p] == 0) {
+      wt[p] = t + 1 - at[p] - bt[p];
+      tat[p] = wt[p] + bt[p];
+      done++;
+    }
+    t++;
+  }
+
+  printf("P AT BT WT TAT\n");
+  for(a = 0; a < n; a++)
+    printf("P%d %2d %2d %2d %3d\n", a+1, at[a], bt[a], wt[a], tat[a]);
+}
