b7c807b5af8c423a93fdb39daeb0a3d6ebac7fb1
Abrar khatib Lajim <88080003+AbrarBb@users.noreply.github.com>
2025-04-22 00:19:27 +0600

    Create fcfs.c

---
+++ OS/Lab-Test/fcfs.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Process:");
+    scanf("%d",&n);
+    int tat[n],wt[n];
+    int bt[]={24,8,5};
+    int at[]={0,0,0};
+    
+    int t=0;
+    for(int a=0;a<n;a++)
+    {
+        if(t<at[a])
+        {
+            t=at[a];
+        }
+        wt[a]=t-at[a];
+        t+=bt[a];
+        tat[a]=wt[a]+bt[a];
+    }
+    
+    printf("\n \tBT\tWT\tTAT\n");
+    for (int a = 0; a < n; a++)
+      {
+       printf("P%d\t%d\t%d\t%d\n", a+1, bt[a], wt[a], tat[a]);
+      }
+       
+
+}
